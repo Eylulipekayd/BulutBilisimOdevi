c2bf9be3aeb030644d1416723fc27f28f8b0c021
Eylulipekayd <eylul.ipek.007@gmail.com>
2026-04-15 14:11:35 +0300

    Dosyalar yüklendi

--- Bulutbilisim/obj/Debug/net10.0/Bulutbilisim.AssemblyInfo.cs
+++ Bulutbilisim/obj/Debug/net10.0/Bulutbilisim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulutbilisim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765f5fede2aa64aab3dfebf9e6162e4c3c4de7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulutbilisim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulutbilisim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
